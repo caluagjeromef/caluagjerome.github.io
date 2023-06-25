88982d6550b8aca3ddbd3a7f64320ea385e2bcb8
Jerome <caluagjeromef@gmail.com>
2023-06-25 17:23:14 +0800

    added project & footer

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ window.onscroll = function() {
       navbar.classList.remove("sticky");
     }
   }
+
+  //slides
+  var slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    var slides = document.getElementsByClassName("slide");
+    
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    
+    setTimeout(showSlides, 2500); // Change slide every 2 seconds (adjust as needed)
+  }
+  
+  function changeSlide(n) {
+    slideIndex += n;
+    showSlides();
+  }
